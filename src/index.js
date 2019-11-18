3fe9fac891c7044a2dbb5b18a7e598ce59d1289f
surya <surya.n@techjini.com>
2019-11-18 20:20:21 +0530

    Updated the schema

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!,
-  data: String
-}
-`
-
-// 2
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    data: () => null
-
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  // 1
+  let idCount = links.length
+  const resolvers = {
+    Query: {
+      info: () => `This is the API of a Hackernews Clone`,
+      feed: () => links,
+    },
+    Mutation: {
+      // 2
+      post: (parent, args) => {
+         const link = {
+          id: `link-${idCount++}`,
+          description: args.description,
+          url: args.url,
+        }
+        links.push(link)
+        return link
+      }
+    },
   }
-}
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
